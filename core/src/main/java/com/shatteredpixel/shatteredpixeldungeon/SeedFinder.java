437a5edfeaad6ff8372f8e43b963efa686098e2a
Fergus Doyle <junkafarian@gmail.com>
2023-01-08 00:17:04 +0000

    log to json instead of readable text

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/SeedFinder.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/SeedFinder.java
@@ -30,21 +30,13 @@ import com.shatteredpixel.shatteredpixeldungeon.items.Gold;
 import com.shatteredpixel.shatteredpixeldungeon.items.Heap;
 import com.shatteredpixel.shatteredpixeldungeon.items.Item;
 import com.shatteredpixel.shatteredpixeldungeon.items.Heap.Type;
-import com.shatteredpixel.shatteredpixeldungeon.items.armor.Armor;
-import com.shatteredpixel.shatteredpixeldungeon.items.artifacts.Artifact;
 import com.shatteredpixel.shatteredpixeldungeon.items.keys.CrystalKey;
 import com.shatteredpixel.shatteredpixeldungeon.items.keys.GoldenKey;
 import com.shatteredpixel.shatteredpixeldungeon.items.keys.IronKey;
-import com.shatteredpixel.shatteredpixeldungeon.items.potions.Potion;
 import com.shatteredpixel.shatteredpixeldungeon.items.quest.CeremonialCandle;
 import com.shatteredpixel.shatteredpixeldungeon.items.quest.CorpseDust;
 import com.shatteredpixel.shatteredpixeldungeon.items.quest.Embers;
 import com.shatteredpixel.shatteredpixeldungeon.items.quest.Pickaxe;
-import com.shatteredpixel.shatteredpixeldungeon.items.rings.Ring;
-import com.shatteredpixel.shatteredpixeldungeon.items.scrolls.Scroll;
-import com.shatteredpixel.shatteredpixeldungeon.items.wands.Wand;
-import com.shatteredpixel.shatteredpixeldungeon.items.weapon.Weapon;
-import com.shatteredpixel.shatteredpixeldungeon.items.weapon.melee.MeleeWeapon;
 import com.shatteredpixel.shatteredpixeldungeon.levels.Level;
 import com.shatteredpixel.shatteredpixeldungeon.utils.DungeonSeed;
 
@@ -58,6 +50,7 @@ public class SeedFinder {
 		public static Condition condition;
 		public static String itemListFile;
 		public static String ouputFile;
+		public static long seed;
 	}
 
 	public class HeapItem {
@@ -76,9 +69,12 @@ public class SeedFinder {
 	// TODO: make it parse the item list directly from the arguments
 	private void setOptions(String[] args) {
 		Options.floors = 4;
-		Options.condition = Condition.ALL;
+		Options.condition = Condition.ANY;
 		Options.itemListFile = "search_items.txt";
 		Options.ouputFile = "seed_items.log";
+		if (args.length == 1) {
+			Options.seed = DungeonSeed.convertFromText(args[0]);
+		}
 
 	}
 
@@ -103,7 +99,6 @@ public class SeedFinder {
 
 	public SeedFinder(String[] args) {
 		setOptions(args);
-		itemList = getItemList();
 
 		try {
 			Writer outputFile = new FileWriter(Options.ouputFile);
@@ -112,13 +107,24 @@ public class SeedFinder {
 			e.printStackTrace();
 		}
 
+		// If a seed has been specified, log the items from that seed and exit
+		// regardless of search items
+		if (args.length == 1) {
+			logSeedItems(Long.toString(Options.seed), Options.floors);
+			return;
+		}
+
+		itemList = getItemList();
+		int matchingSeeds = 0;
+
 		// for (int i = 0; i < DungeonSeed.TOTAL_SEEDS; i++) {
 		for (int i = 0; i < 5000; i++) {
 			if (i % 1000 == 0) {
-				System.out.printf("Checked %s seeds\n", Integer.toString(i));
+				System.out.printf("Checked %d seeds, found %d matching seeds\n", i, matchingSeeds);
+				matchingSeeds = 0;
 			}
 			if (testSeed(Integer.toString(i), Options.floors)) {
-				System.out.printf("Found valid seed %s (%d)\n", DungeonSeed.convertToCode(Dungeon.seed), Dungeon.seed);
+				matchingSeeds++;
 				logSeedItems(Integer.toString(i), Options.floors);
 			}
 		}
@@ -220,51 +226,11 @@ public class SeedFinder {
 
 	// Formatting and output
 
-	private void addTextItems(String caption, ArrayList<HeapItem> items, StringBuilder builder) {
-		if (items.isEmpty()) {
-			// No need to process heap
-			return;
-		}
-
-		builder.append(caption + ":\n");
-
-		for (HeapItem item : items) {
-			Item i = item.item;
-			Heap h = item.heap;
-
-			if (((i instanceof Armor && ((Armor) i).hasGoodGlyph()) ||
-					(i instanceof Weapon && ((Weapon) i).hasGoodEnchant()) ||
-					(i instanceof Ring) || (i instanceof Wand)) && i.cursed)
-				builder.append("- cursed " + i.title().toLowerCase());
-
-			else
-				builder.append("- " + i.title().toLowerCase());
-
-			if (h.type != Type.HEAP)
-				builder.append(" (" + h.title().toLowerCase() + ")");
-
-			builder.append("\n");
-		}
-
-		builder.append("\n");
-	}
-
-	private void addTextQuest(String caption, ArrayList<Item> items, StringBuilder builder) {
-		if (!items.isEmpty()) {
-			return;
-		}
-
-		builder.append(caption + ":\n");
-
-		for (Item i : items) {
-			if (i.cursed)
-				builder.append("- cursed " + i.title().toLowerCase() + "\n");
-
-			else
-				builder.append("- " + i.title().toLowerCase() + "\n");
-		}
-
-		builder.append("\n");
+	private void logItem(Item item, String found_in, PrintWriter out) {
+		out.printf(
+				"{\"seed\": \"%s\", \"name\": \"%s\", \"floor\": %d, \"found_in\": \"%s\", \"is_cursed\": %b, \"item_level\": %d, \"type\": \"%s\"}\n",
+				DungeonSeed.convertToCode(Dungeon.seed), item.title().toLowerCase(), Dungeon.depth,
+				found_in, item.cursed, item.level(), item.getClass().getSimpleName());
 	}
 
 	private void logSeedItems(String seed, int floors) {
@@ -283,100 +249,43 @@ public class SeedFinder {
 		blacklist = Arrays.asList(Gold.class, Dewdrop.class, IronKey.class, GoldenKey.class, CrystalKey.class,
 				EnergyCrystal.class, CorpseDust.class, Embers.class, CeremonialCandle.class, Pickaxe.class);
 
-		out.printf("Items for seed %s (%d):\n\n", DungeonSeed.convertToCode(Dungeon.seed), Dungeon.seed);
-
 		for (int i = 0; i < floors; i++) {
-			out.printf("--- floor %d ---\n\n", Dungeon.depth);
-
 			Level l = Dungeon.newLevel();
 			ArrayList<Heap> heaps = new ArrayList<>(l.heaps.valueList());
-			StringBuilder builder = new StringBuilder();
-			ArrayList<HeapItem> scrolls = new ArrayList<>();
-			ArrayList<HeapItem> potions = new ArrayList<>();
-			ArrayList<HeapItem> equipment = new ArrayList<>();
-			ArrayList<HeapItem> rings = new ArrayList<>();
-			ArrayList<HeapItem> artifacts = new ArrayList<>();
-			ArrayList<HeapItem> wands = new ArrayList<>();
-			ArrayList<HeapItem> others = new ArrayList<>();
 
 			// list quest rewards
 			if (Ghost.Quest.armor != null) {
-				ArrayList<Item> rewards = new ArrayList<>();
-				rewards.add(Ghost.Quest.armor.identify());
-				rewards.add(Ghost.Quest.weapon.identify());
+				logItem(Ghost.Quest.armor.identify(), "Ghost", out);
+				logItem(Ghost.Quest.weapon.identify(), "Ghost", out);
 				Ghost.Quest.complete();
-
-				addTextQuest("Ghost quest rewards", rewards, builder);
 			}
 
 			if (Wandmaker.Quest.wand1 != null) {
-				ArrayList<Item> rewards = new ArrayList<>();
-				rewards.add(Wandmaker.Quest.wand1.identify());
-				rewards.add(Wandmaker.Quest.wand2.identify());
+				logItem(Wandmaker.Quest.wand1.identify(), "Wandmaker", out);
+				logItem(Wandmaker.Quest.wand2.identify(), "Wandmaker", out);
 				Wandmaker.Quest.complete();
-
-				builder.append("Wandmaker quest item: ");
-
-				switch (Wandmaker.Quest.type) {
-					case 1:
-					default:
-						builder.append("corpse dust\n\n");
-						break;
-					case 2:
-						builder.append("fresh embers\n\n");
-						break;
-					case 3:
-						builder.append("rotberry seed\n\n");
-				}
-
-				addTextQuest("Wandmaker quest rewards", rewards, builder);
 			}
 
 			if (Imp.Quest.reward != null) {
-				ArrayList<Item> rewards = new ArrayList<>();
-				rewards.add(Imp.Quest.reward.identify());
+				logItem(Imp.Quest.reward.identify(), "Imp", out);
 				Imp.Quest.complete();
-
-				addTextQuest("Imp quest reward", rewards, builder);
 			}
 
 			heaps.addAll(getMobDrops(l));
 
 			// list items
-			for (Heap h : heaps) {
-				for (Item item : h.items) {
+			for (Heap heap : heaps) {
+				for (Item item : heap.items) {
 					item.identify();
 
-					if (h.type == Type.FOR_SALE)
+					if (heap.type == Type.FOR_SALE)
 						continue;
 					else if (blacklist.contains(item.getClass()))
 						continue;
-					else if (item instanceof Scroll)
-						scrolls.add(new HeapItem(item, h));
-					else if (item instanceof Potion)
-						potions.add(new HeapItem(item, h));
-					else if (item instanceof MeleeWeapon || item instanceof Armor)
-						equipment.add(new HeapItem(item, h));
-					else if (item instanceof Ring)
-						rings.add(new HeapItem(item, h));
-					else if (item instanceof Artifact)
-						artifacts.add(new HeapItem(item, h));
-					else if (item instanceof Wand)
-						wands.add(new HeapItem(item, h));
 					else
-						others.add(new HeapItem(item, h));
+						logItem(item, heap.title().toLowerCase(), out);
 				}
 			}
-
-			addTextItems("Scrolls", scrolls, builder);
-			addTextItems("Potions", potions, builder);
-			addTextItems("Equipment", equipment, builder);
-			addTextItems("Rings", rings, builder);
-			addTextItems("Artifacts", artifacts, builder);
-			addTextItems("Wands", wands, builder);
-			addTextItems("Other", others, builder);
-
-			out.print(builder.toString());
 
 			Dungeon.depth++;
 		}
